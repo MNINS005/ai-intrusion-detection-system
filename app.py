3c21fde7e2a4ea0ad01e69b513e190deb7d0751d
MNINS005 <nikhil06012005@gmail.com>
2026-05-03 21:58:05 +0530

    add runtime.txt

--- app.py
+++ app.py
@@ -134,4 +134,5 @@ def predict(data: SimpleInput):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
